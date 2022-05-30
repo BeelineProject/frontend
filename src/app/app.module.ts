c5c3eaedc3511a9538201bd30e5590c8a0c7bb14
asmaSmida <asmasmida@insat.u-carthage.tn>
2022-05-30 19:50:49 +0100

    register nd sign in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+//import { authInterceptorProviders } from 'src/_helpers/auth.interceptor';
+
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    LoginComponent,
+    RegisterComponent,
+    NavBarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+ // providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
